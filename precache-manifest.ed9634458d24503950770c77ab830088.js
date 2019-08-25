aa8ea74b281fae2c25d1b3f12dfbb42e89df998d
parkchanjong <7000cj@gmail.com>
2019-08-25 22:23:06 +0900

    Updates

---
+++ precache-manifest.ed9634458d24503950770c77ab830088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cca6e570d823b922fa331aba066f564",
+    "url": "/dariplan/index.html"
+  },
+  {
+    "revision": "c9f7a72ac9412b666a21",
+    "url": "/dariplan/static/js/2.417d6faf.chunk.js"
+  },
+  {
+    "revision": "30127b4744993be46ce2",
+    "url": "/dariplan/static/js/main.b41b9eb2.chunk.js"
+  },
+  {
+    "revision": "472b4bce5ec10445a524",
+    "url": "/dariplan/static/js/runtime~main.06c9ee50.js"
+  },
+  {
+    "revision": "f6a64c55c8de0b69588ce39de5b70f7c",
+    "url": "/dariplan/static/media/about.f6a64c55.jpg"
+  },
+  {
+    "revision": "97c61f0f86876544a540cfa2d54b3948",
+    "url": "/dariplan/static/media/bg.97c61f0f.jpg"
+  }
+]);
