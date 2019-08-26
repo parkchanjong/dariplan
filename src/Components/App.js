8c85ea35b4c6735bdd1aac86faa7ee0a7682af81
parkchanjong <7000cj@gmail.com>
2019-08-26 19:29:55 +0900

    react _ga test

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,9 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from '../Styles/Theme';
 import Mainpage from './main';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-119130828-2');
 
 export default () => {
   return (
